b721ae3afd5b606532cc46c0f0d6985ae3d3b7f9
NoriX <versatility.95@mail.ru>
2021-09-13 12:56:57 +0300

    Running bundled code in iframe.

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
   const ref = useRef<any>();
+  const iframeRef = useRef<any>();
 
   useEffect(() => {
     startService();
@@ -25,6 +25,8 @@ const App = () => {
       return;
     }
 
+    iframeRef.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -36,8 +38,37 @@ const App = () => {
       },
     });
 
-    setCode(result.outputFiles[0].text);
+    iframeRef.current.contentWindow.postMessage(
+      result.outputFiles[0].text,
+      '*'
+    );
   };
+
+  const html = `
+    <!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+      </head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            }catch(err){
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
+            }
+          }, false)
+        </script>
+      </body>
+    </html>
+  `;
 
   return (
     <div>
@@ -45,7 +76,12 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe
+        ref={iframeRef}
+        srcDoc={html}
+        title="frame"
+        sandbox="allow-scripts"
+      />
     </div>
   );
 };

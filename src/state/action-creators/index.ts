f81becf75a35c45d19fe719b3d57694446f4667a
NoriX <versatility.95@mail.ru>
2021-10-01 17:33:51 +0300

    Actions for fetching and saving cells

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,4 +1,5 @@
 import { Dispatch } from 'redux';
+import axios from 'axios';
 import { ActionTypes } from '../action-types';
 import {
   Action,
@@ -8,8 +9,9 @@ import {
   MoveCellAction,
   UpdateCellAction,
 } from '../actions';
-import { CellTypes } from '../cell';
+import { Cell, CellTypes } from '../cell';
 import bundler from '../../bundler';
+import { RootState } from '../reducers';
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
   return {
@@ -71,4 +73,40 @@ export const createBundle =
         bundle: result,
       },
     });
+  };
+
+export const fetchCells = () => async (dispatch: Dispatch<Action>) => {
+  dispatch({ type: ActionTypes.FETCH_CELLS });
+
+  try {
+    const { data }: { data: Cell[] } = await axios.get('/cells');
+
+    dispatch({
+      type: ActionTypes.FETCH_CELLS_COMPLETE,
+      payload: data,
+    });
+  } catch (err: any) {
+    dispatch({
+      type: ActionTypes.FETCH_CELLS_ERROR,
+      payload: err.message,
+    });
+  }
+};
+
+export const saveCells =
+  () => async (dispatch: Dispatch<Action>, getState: () => RootState) => {
+    const {
+      cells: { data, order },
+    } = getState();
+
+    const cells = order.map((id) => data[id]);
+
+    try {
+      await axios.post('/cells', { cells });
+    } catch (err: any) {
+      dispatch({
+        type: ActionTypes.SAVE_CELLS_ERROR,
+        payload: err.message,
+      });
+    }
   };
